8cf98f16e955e290404cf5810f008600c02c07f1
chhote04 <chhotelaljatav143@gmail.com>
2022-11-05 17:08:24 +0530

    files

--- resources/views/front/master.blade.php
+++ resources/views/front/master.blade.php
@@ -1,10 +1,9 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8">
-    <title>InfinityShop - Online Shop Website Template</title>
+    <title>AsGardian - Online Shop </title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="Free HTML Templates" name="keywords">
     <meta content="Free HTML Templates" name="description">
